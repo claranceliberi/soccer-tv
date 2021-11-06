c9912a4884a34964ab38d199bf8db592b961be42
claranceliberi <liberintwari@gmail.com>
2021-11-06 19:28:18 +0200

    feat: first tev

--- App.vue
+++ App.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-     <iframe frameborder="0" marginheight="0" marginwidth="0" height="600" allowfullscreen="true" src="http://uhdstreams.club/footy/ch1.php" scrolling="no" width="900" name="iframe1">Your browser does not support iframes</iframe><script id="cid0020000199407514170" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 320px;height: 600px;">{"handle":"hdstreamsclub","arch":"js","styles":{"a":"ff9900","b":100,"c":"000000","d":"000000","k":"ff9900","l":"ff9900","m":"ff9900","p":"10","q":"ff9900","r":100,"ab":false,"surl":0,"cnrs":"0.35","fwtickm":1}}</script>
+    <iframe
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      height="600"
+      allowfullscreen="true"
+      src="http://uhdstreams.club/footy/ch1.php"
+      scrolling="no"
+      width="900"
+      name="iframe1"
+      >Your browser does not support iframes</iframe
+    >
   </div>
 </template>
