a455d76f7b1a2e2f8e9d79449fb7389639ddd21d
claranceliberi <liberintwari@gmail.com>
2021-11-06 22:07:52 +0200

    fix: allow http over https

--- App.vue
+++ App.vue
@@ -2,21 +2,9 @@
   <main>
     <header>
       <nav>
-        <button @click="changeChannel(1)">CH 1</button>
-        <button @click="changeChannel(2)">CH 2</button>
-        <button @click="changeChannel(3)">CH 3</button>
-        <button @click="changeChannel(4)">CH 4</button>
-        <button @click="changeChannel(5)">CH 5</button>
-        <button @click="changeChannel(6)">CH 6</button>
-        <button @click="changeChannel(7)">CH 7</button>
-        <button @click="changeChannel(8)">CH 8</button>
-        <button @click="changeChannel(9)">CH 9</button>
-        <button @click="changeChannel(10)">CH 10</button>
-        <button @click="changeChannel(11)">CH 11</button>
-        <button @click="changeChannel(12)">CH 12</button>
-        <button @click="changeChannel(13)">CH 13</button>
-        <button @click="changeChannel(14)">CH 14</button>
-        <button @click="changeChannel(15)">CH 15</button>
+        <button @click="changeChannel(i)" v-for="i in 15" :key="i">
+          CH {{ i }}
+        </button>
       </nav>
     </header>
     <section>
@@ -36,6 +24,16 @@
 </template>
 
 <script lang="ts" setup>
+useMeta({
+  title: "Soccer Tv",
+  meta: [
+    {
+      "http-equiv": "Content-Security-Policy",
+      content: "upgrade-insecure-requests",
+    },
+  ],
+});
+
 const activeChannel = ref(1);
 
 function changeChannel(channel: number) {
